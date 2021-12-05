5b63d4331d5c7f32cc02eff2ce8d4492dfab4bd8
Nadzeya Hryshalevich <nhryshalevich@solidex.by>
2021-12-05 16:13:18 +0300

    Update 'anomaly_detector/storage/__init__.py'
    (cherry picked from commit 07bd9b37da0402cad44cfaea5326f52e171d232c)

--- anomaly_detector/storage/__init__.py
+++ anomaly_detector/storage/__init__.py
@@ -5,7 +5,9 @@ from anomaly_detector.storage.local_directory_storage import LocalDirStorage
 from anomaly_detector.storage.storage_attribute import DefaultStorageAttribute, ESStorageAttribute
 from anomaly_detector.storage.kafka_storage import KafkaSink
 from anomaly_detector.storage.storage_catalog import StorageCatalog
+from anomaly_detector.storage.mongodb_storage import MongoDBStorage
 
 __all__ = ['ESStorage', 'DefaultStorageAttribute',
            'ESStorageAttribute', 'LocalDirStorage', 'KafkaSink',
-           'LocalStorageDataSink', 'LocalStorageDataSource', 'StorageCatalog']
+           'LocalStorageDataSink', 'LocalStorageDataSource', 'StorageCatalog',
+           'MongoDBStorage']
