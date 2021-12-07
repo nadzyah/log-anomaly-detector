362975d18df8158904d479dc084a4abb1e53ea4d
nadzyah <nadzya.info@gmail.com>
2021-12-07 15:56:00 +0300

    v0.2.0
    (cherry picked from commit 02bad32fe949207cd2e720d533e197f3b81054e6)

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ REQUIRED_PKG = [
 
 setup(
     name="log-anomaly-detector",
-    version="0.1.10",
+    version="0.2.0",
     py_modules=['app'],
     packages=find_packages(),
     setup_requires=["pytest-runner"],
